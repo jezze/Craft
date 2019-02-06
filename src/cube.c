a7fa190c28910d0141a3f40227ef67fb82a294e0
Jens Nyberg <jens.nyberg@gmail.com>
2019-02-06 21:09:32 +0100

    more cleanups
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/cube.c
+++ src/cube.c
@@ -4,12 +4,9 @@
 #include "matrix.h"
 #include "util.h"
 
-void make_cube_faces(
-    float *data, float ao[6][4], float light[6][4],
-    int left, int right, int top, int bottom, int front, int back,
-    int wleft, int wright, int wtop, int wbottom, int wfront, int wback,
-    float x, float y, float z, float n)
+void make_cube_faces(float *data, float ao[6][4], float light[6][4], int left, int right, int top, int bottom, int front, int back, int wleft, int wright, int wtop, int wbottom, int wfront, int wback, float x, float y, float z, float n)
 {
+
     static const float positions[6][4][3] = {
         {{-1, -1, -1}, {-1, -1, +1}, {-1, +1, -1}, {-1, +1, +1}},
         {{+1, -1, -1}, {+1, -1, +1}, {+1, +1, -1}, {+1, +1, +1}},
@@ -18,6 +15,7 @@ void make_cube_faces(
         {{-1, -1, -1}, {-1, +1, -1}, {+1, -1, -1}, {+1, +1, -1}},
         {{-1, -1, +1}, {-1, +1, +1}, {+1, -1, +1}, {+1, +1, +1}}
     };
+
     static const float normals[6][3] = {
         {-1, 0, 0},
         {+1, 0, 0},
@@ -26,6 +24,7 @@ void make_cube_faces(
         {0, 0, -1},
         {0, 0, +1}
     };
+
     static const float uvs[6][4][2] = {
         {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
         {{1, 0}, {0, 0}, {1, 1}, {0, 1}},
@@ -34,6 +33,7 @@ void make_cube_faces(
         {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
         {{1, 0}, {1, 1}, {0, 0}, {0, 1}}
     };
+
     static const float indices[6][6] = {
         {0, 3, 2, 0, 1, 3},
         {0, 3, 1, 0, 2, 3},
@@ -42,6 +42,7 @@ void make_cube_faces(
         {0, 3, 2, 0, 1, 3},
         {0, 3, 1, 0, 2, 3}
     };
+
     static const float flipped[6][6] = {
         {0, 1, 2, 1, 3, 2},
         {0, 2, 1, 2, 3, 1},
@@ -50,21 +51,29 @@ void make_cube_faces(
         {0, 1, 2, 1, 3, 2},
         {0, 2, 1, 2, 3, 1}
     };
+
     float *d = data;
     float s = 0.0625;
     float a = 0 + 1 / 2048.0;
     float b = s - 1 / 2048.0;
     int faces[6] = {left, right, top, bottom, front, back};
     int tiles[6] = {wleft, wright, wtop, wbottom, wfront, wback};
-    for (int i = 0; i < 6; i++) {
-        if (faces[i] == 0) {
+
+    for (int i = 0; i < 6; i++)
+    {
+
+        if (faces[i] == 0)
             continue;
-        }
+
         float du = (tiles[i] % 16) * s;
         float dv = (tiles[i] / 16) * s;
         int flip = ao[i][0] + ao[i][3] > ao[i][1] + ao[i][2];
-        for (int v = 0; v < 6; v++) {
+
+        for (int v = 0; v < 6; v++)
+        {
+
             int j = flip ? flipped[i][v] : indices[i][v];
+
             *(d++) = x + n * positions[i][j][0];
             *(d++) = y + n * positions[i][j][1];
             *(d++) = z + n * positions[i][j][2];
@@ -75,65 +84,73 @@ void make_cube_faces(
             *(d++) = dv + (uvs[i][j][1] ? b : a);
             *(d++) = ao[i][j];
             *(d++) = light[i][j];
+
         }
+
     }
+
 }
 
-void make_cube(
-    float *data, float ao[6][4], float light[6][4],
-    int left, int right, int top, int bottom, int front, int back,
-    float x, float y, float z, float n, int w)
+void make_cube(float *data, float ao[6][4], float light[6][4], int left, int right, int top, int bottom, int front, int back, float x, float y, float z, float n, int w)
 {
+
     int wleft = blocks[w][0];
     int wright = blocks[w][1];
     int wtop = blocks[w][2];
     int wbottom = blocks[w][3];
     int wfront = blocks[w][4];
     int wback = blocks[w][5];
-    make_cube_faces(
-        data, ao, light,
-        left, right, top, bottom, front, back,
-        wleft, wright, wtop, wbottom, wfront, wback,
-        x, y, z, n);
+    
+    make_cube_faces(data, ao, light, left, right, top, bottom, front, back, wleft, wright, wtop, wbottom, wfront, wback, x, y, z, n);
+
 }
 
-void make_plant(
-    float *data, float ao, float light,
-    float px, float py, float pz, float n, int w, float rotation)
+void make_plant(float *data, float ao, float light, float px, float py, float pz, float n, int w, float rotation)
 {
+
     static const float positions[4][4][3] = {
         {{ 0, -1, -1}, { 0, -1, +1}, { 0, +1, -1}, { 0, +1, +1}},
         {{ 0, -1, -1}, { 0, -1, +1}, { 0, +1, -1}, { 0, +1, +1}},
         {{-1, -1,  0}, {-1, +1,  0}, {+1, -1,  0}, {+1, +1,  0}},
         {{-1, -1,  0}, {-1, +1,  0}, {+1, -1,  0}, {+1, +1,  0}}
     };
+
     static const float normals[4][3] = {
         {-1, 0, 0},
         {+1, 0, 0},
         {0, 0, -1},
         {0, 0, +1}
     };
+
     static const float uvs[4][4][2] = {
         {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
         {{1, 0}, {0, 0}, {1, 1}, {0, 1}},
         {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
         {{1, 0}, {1, 1}, {0, 0}, {0, 1}}
     };
+
     static const float indices[4][6] = {
         {0, 3, 2, 0, 1, 3},
         {0, 3, 1, 0, 2, 3},
         {0, 3, 2, 0, 1, 3},
         {0, 3, 1, 0, 2, 3}
     };
+
     float *d = data;
     float s = 0.0625;
     float a = 0;
     float b = s;
     float du = (plants[w] % 16) * s;
     float dv = (plants[w] / 16) * s;
-    for (int i = 0; i < 4; i++) {
-        for (int v = 0; v < 6; v++) {
+
+    for (int i = 0; i < 4; i++)
+    {
+
+        for (int v = 0; v < 6; v++)
+        {
+
             int j = indices[i][v];
+
             *(d++) = n * positions[i][j][0];
             *(d++) = n * positions[i][j][1];
             *(d++) = n * positions[i][j][2];
@@ -144,10 +161,14 @@ void make_plant(
             *(d++) = dv + (uvs[i][j][1] ? b : a);
             *(d++) = ao;
             *(d++) = light;
+
         }
+
     }
+
     float ma[16];
     float mb[16];
+
     mat_identity(ma);
     mat_rotate(mb, 0, 1, 0, RADIANS(rotation));
     mat_multiply(ma, mb, ma);
@@ -155,12 +176,12 @@ void make_plant(
     mat_translate(mb, px, py, pz);
     mat_multiply(ma, mb, ma);
     mat_apply(data, ma, 24, 0, 10);
+
 }
 
-void make_player(
-    float *data,
-    float x, float y, float z, float rx, float ry)
+void make_player(float *data, float x, float y, float z, float rx, float ry)
 {
+
     float ao[6][4] = {0};
     float light[6][4] = {
         {0.8, 0.8, 0.8, 0.8},
@@ -170,13 +191,12 @@ void make_player(
         {0.8, 0.8, 0.8, 0.8},
         {0.8, 0.8, 0.8, 0.8}
     };
-    make_cube_faces(
-        data, ao, light,
-        1, 1, 1, 1, 1, 1,
-        226, 224, 241, 209, 225, 227,
-        0, 0, 0, 0.4);
+
+    make_cube_faces(data, ao, light, 1, 1, 1, 1, 1, 1, 226, 224, 241, 209, 225, 227, 0, 0, 0, 0.4);
+
     float ma[16];
     float mb[16];
+
     mat_identity(ma);
     mat_rotate(mb, 0, 1, 0, rx);
     mat_multiply(ma, mb, ma);
@@ -186,12 +206,12 @@ void make_player(
     mat_translate(mb, x, y, z);
     mat_multiply(ma, mb, ma);
     mat_apply(data, ma, 36, 0, 10);
+
 }
 
-void make_character(
-    float *data,
-    float x, float y, float n, float m, char c)
+void make_character(float *data, float x, float y, float n, float m, char c)
 {
+
     float *d = data;
     float s = 0.0625;
     float a = s;
@@ -199,6 +219,7 @@ void make_character(
     int w = c - 32;
     float du = (w % 16) * a;
     float dv = 1 - (w / 16) * b - b;
+
     *(d++) = x - n; *(d++) = y - m;
     *(d++) = du + 0; *(d++) = dv;
     *(d++) = x + n; *(d++) = y - m;
@@ -211,11 +232,12 @@ void make_character(
     *(d++) = du + a; *(d++) = dv + b;
     *(d++) = x - n; *(d++) = y + m;
     *(d++) = du + 0; *(d++) = dv + b;
+
 }
 
-void make_character_3d(
-    float *data, float x, float y, float z, float n, int face, char c)
+void make_character_3d(float *data, float x, float y, float z, float n, int face, char c)
 {
+
     static const float positions[8][6][3] = {
         {{0, -2, -1}, {0, +2, +1}, {0, +2, -1},
          {0, -2, -1}, {0, -2, +1}, {0, +2, +1}},
@@ -234,6 +256,7 @@ void make_character_3d(
         {{+2, 0, -1}, {-2, 0, +1}, {+2, 0, +1},
          {+2, 0, -1}, {-2, 0, -1}, {-2, 0, +1}}
     };
+
     static const float uvs[8][6][2] = {
         {{0, 0}, {1, 1}, {0, 1}, {0, 0}, {1, 0}, {1, 1}},
         {{1, 0}, {0, 1}, {0, 0}, {1, 0}, {1, 1}, {0, 1}},
@@ -244,10 +267,12 @@ void make_character_3d(
         {{0, 1}, {1, 0}, {1, 1}, {0, 1}, {0, 0}, {1, 0}},
         {{0, 1}, {1, 0}, {1, 1}, {0, 1}, {0, 0}, {1, 0}}
     };
+
     static const float offsets[8][3] = {
         {-1, 0, 0}, {+1, 0, 0}, {0, 0, -1}, {0, 0, +1},
         {0, +1, 0}, {0, +1, 0}, {0, +1, 0}, {0, +1, 0},
     };
+
     float *d = data;
     float s = 0.0625;
     float pu = s / 5;
@@ -260,25 +285,32 @@ void make_character_3d(
     int w = c - 32;
     float du = (w % 16) * s;
     float dv = 1 - (w / 16 + 1) * s * 2;
+
     x += p * offsets[face][0];
     y += p * offsets[face][1];
     z += p * offsets[face][2];
-    for (int i = 0; i < 6; i++) {
+
+    for (int i = 0; i < 6; i++)
+    {
+
         *(d++) = x + n * positions[face][i][0];
         *(d++) = y + n * positions[face][i][1];
         *(d++) = z + n * positions[face][i][2];
         *(d++) = du + (uvs[face][i][0] ? u2 : u1);
         *(d++) = dv + (uvs[face][i][1] ? v2 : v1);
+
     }
+
 }
 
-int _make_sphere(
-    float *data, float r, int detail,
-    float *a, float *b, float *c,
-    float *ta, float *tb, float *tc)
+int _make_sphere(float *data, float r, int detail, float *a, float *b, float *c, float *ta, float *tb, float *tc)
 {
-    if (detail == 0) {
+
+    if (detail == 0)
+    {
+
         float *d = data;
+
         *(d++) = a[0] * r; *(d++) = a[1] * r; *(d++) = a[2] * r;
         *(d++) = a[0]; *(d++) = a[1]; *(d++) = a[2];
         *(d++) = ta[0]; *(d++) = ta[1];
@@ -288,24 +320,38 @@ int _make_sphere(
         *(d++) = c[0] * r; *(d++) = c[1] * r; *(d++) = c[2] * r;
         *(d++) = c[0]; *(d++) = c[1]; *(d++) = c[2];
         *(d++) = tc[0]; *(d++) = tc[1];
+
         return 1;
+
     }
-    else {
+
+    else
+    {
+
         float ab[3], ac[3], bc[3];
-        for (int i = 0; i < 3; i++) {
+
+        for (int i = 0; i < 3; i++)
+        {
+
             ab[i] = (a[i] + b[i]) / 2;
             ac[i] = (a[i] + c[i]) / 2;
             bc[i] = (b[i] + c[i]) / 2;
+
         }
+
         normalize(ab + 0, ab + 1, ab + 2);
         normalize(ac + 0, ac + 1, ac + 2);
         normalize(bc + 0, bc + 1, bc + 2);
+
         float tab[2], tac[2], tbc[2];
+
         tab[0] = 0; tab[1] = 1 - acosf(ab[1]) / PI;
         tac[0] = 0; tac[1] = 1 - acosf(ac[1]) / PI;
         tbc[0] = 0; tbc[1] = 1 - acosf(bc[1]) / PI;
+
         int total = 0;
         int n;
+
         n = _make_sphere(data, r, detail - 1, a, ab, ac, ta, tab, tac);
         total += n; data += n * 24;
         n = _make_sphere(data, r, detail - 1, b, bc, ab, tb, tbc, tab);
@@ -314,46 +360,46 @@ int _make_sphere(
         total += n; data += n * 24;
         n = _make_sphere(data, r, detail - 1, ab, bc, ac, tab, tbc, tac);
         total += n; data += n * 24;
+
         return total;
+
     }
+
 }
 
-void make_sphere(float *data, float r, int detail) {
-    // detail, triangles, floats
-    // 0, 8, 192
-    // 1, 32, 768
-    // 2, 128, 3072
-    // 3, 512, 12288
-    // 4, 2048, 49152
-    // 5, 8192, 196608
-    // 6, 32768, 786432
-    // 7, 131072, 3145728
+void make_sphere(float *data, float r, int detail)
+{
+
     static int indices[8][3] = {
         {4, 3, 0}, {1, 4, 0},
         {3, 4, 5}, {4, 1, 5},
         {0, 3, 2}, {0, 2, 1},
         {5, 2, 3}, {5, 1, 2}
     };
+
     static float positions[6][3] = {
         { 0, 0,-1}, { 1, 0, 0},
         { 0,-1, 0}, {-1, 0, 0},
         { 0, 1, 0}, { 0, 0, 1}
     };
+
     static float uvs[6][3] = {
         {0, 0.5}, {0, 0.5},
         {0, 0}, {0, 0.5},
         {0, 1}, {0, 0.5}
     };
+
     int total = 0;
-    for (int i = 0; i < 8; i++) {
-        int n = _make_sphere(
-            data, r, detail,
-            positions[indices[i][0]],
-            positions[indices[i][1]],
-            positions[indices[i][2]],
-            uvs[indices[i][0]],
-            uvs[indices[i][1]],
-            uvs[indices[i][2]]);
-        total += n; data += n * 24;
+
+    for (int i = 0; i < 8; i++)
+    {
+
+        int n = _make_sphere(data, r, detail, positions[indices[i][0]], positions[indices[i][1]], positions[indices[i][2]], uvs[indices[i][0]], uvs[indices[i][1]], uvs[indices[i][2]]);
+
+        total += n;
+        data += n * 24;
+
     }
+    
 }
+
