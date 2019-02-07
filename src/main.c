e2f45c500d7bfe649d3d9452f3d3f84cb64bbaac
Jens Nyberg <jens.nyberg@gmail.com>
2019-02-07 22:14:40 +0100

    remove workers
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/main.c
+++ src/main.c
@@ -16,14 +16,10 @@
 #include "util.h"
 
 #define MAX_CHUNKS 8192
-#define WORKERS 4
 #define MAX_TEXT_LENGTH 256
 #define ALIGN_LEFT 0
 #define ALIGN_CENTER 1
 #define ALIGN_RIGHT 2
-#define WORKER_IDLE 0
-#define WORKER_BUSY 1
-#define WORKER_DONE 2
 
 typedef struct
 {
@@ -63,18 +59,6 @@ typedef struct
     GLfloat *data;
 
 } WorkerItem;
-
-typedef struct
-{
-
-    int index;
-    int state;
-    thrd_t thrd;
-    mtx_t mtx;
-    cnd_t cnd;
-    WorkerItem item;
-
-} Worker;
 
 typedef struct
 {
@@ -119,7 +103,6 @@ typedef struct
 {
 
     GLFWwindow *window;
-    Worker workers[WORKERS];
     Chunk chunks[MAX_CHUNKS];
     int chunk_count;
     int create_radius;
@@ -875,8 +858,6 @@ static void compute_chunk(WorkerItem *item)
     int ox = item->p * CHUNK_SIZE - CHUNK_SIZE - 1;
     int oy = -1;
     int oz = item->q * CHUNK_SIZE - CHUNK_SIZE - 1;
-
-    // check for lights
     int has_light = 0;
 
     if (SHOW_LIGHTS)
@@ -899,7 +880,6 @@ static void compute_chunk(WorkerItem *item)
 
     }
 
-    // populate opaque array
     for (int a = 0; a < 3; a++)
     {
 
@@ -939,7 +919,6 @@ static void compute_chunk(WorkerItem *item)
 
     }
 
-    // flood fill light intensities
     if (has_light)
     {
 
@@ -1406,81 +1385,6 @@ static void delete_all_chunks()
 
 }
 
-static void check_workers()
-{
-
-    for (int i = 0; i < WORKERS; i++)
-    {
-
-        Worker *worker = g->workers + i;
-
-        mtx_lock(&worker->mtx);
-
-        if (worker->state == WORKER_DONE)
-        {
-
-            WorkerItem *item = &worker->item;
-            Chunk *chunk = find_chunk(item->p, item->q);
-
-            if (chunk)
-            {
-
-                if (item->load)
-                {
-
-                    Map *block_map = item->block_maps[1][1];
-                    Map *light_map = item->light_maps[1][1];
-
-                    map_free(&chunk->map);
-                    map_free(&chunk->lights);
-                    map_copy(&chunk->map, block_map);
-                    map_copy(&chunk->lights, light_map);
-
-                }
-
-                generate_chunk(chunk, item);
-
-            }
-
-            for (int a = 0; a < 3; a++)
-            {
-
-                for (int b = 0; b < 3; b++)
-                {
-
-                    Map *block_map = item->block_maps[a][b];
-                    Map *light_map = item->light_maps[a][b];
-
-                    if (block_map)
-                    {
-
-                        map_free(block_map);
-                        free(block_map);
-
-                    }
-
-                    if (light_map)
-                    {
-
-                        map_free(light_map);
-                        free(light_map);
-
-                    }
-
-                }
-
-            }
-
-            worker->state = WORKER_IDLE;
-
-        }
-
-        mtx_unlock(&worker->mtx);
-
-    }
-
-}
-
 static void force_chunks(Player *player)
 {
 
@@ -1519,206 +1423,6 @@ static void force_chunks(Player *player)
         }
 
     }
-
-}
-
-static void ensure_chunks_worker(Player *player, Worker *worker)
-{
-
-    float matrix[16];
-
-    set_matrix_3d(matrix, g->width, g->height, player->x, player->y, player->z, player->rx, player->ry, g->fov, g->ortho, g->render_radius);
-
-    float planes[6][4];
-
-    frustum_planes(planes, g->render_radius, matrix);
-
-    int p = chunked(player->x);
-    int q = chunked(player->z);
-    int r = g->create_radius;
-
-    int start = 0x0fffffff;
-    int best_score = start;
-    int best_a = 0;
-    int best_b = 0;
-
-    for (int dp = -r; dp <= r; dp++)
-    {
-
-        for (int dq = -r; dq <= r; dq++)
-        {
-
-            int a = p + dp;
-            int b = q + dq;
-            int index = (ABS(a) ^ ABS(b)) % WORKERS;
-
-            if (index != worker->index)
-                continue;
-
-            Chunk *chunk = find_chunk(a, b);
-
-            if (chunk && !chunk->dirty)
-                continue;
-
-            int distance = MAX(ABS(dp), ABS(dq));
-            int invisible = !chunk_visible(planes, a, b, 0, 256);
-            int priority = 0;
-
-            if (chunk)
-                priority = chunk->buffer && chunk->dirty;
-
-            int score = (invisible << 24) | (priority << 16) | distance;
-
-            if (score < best_score)
-            {
-
-                best_score = score;
-                best_a = a;
-                best_b = b;
-
-            }
-
-        }
-
-    }
-
-    if (best_score == start)
-        return;
-
-    int a = best_a;
-    int b = best_b;
-    int load = 0;
-    Chunk *chunk = find_chunk(a, b);
-
-    if (!chunk)
-    {
-
-        load = 1;
-
-        if (g->chunk_count < MAX_CHUNKS)
-        {
-
-            chunk = g->chunks + g->chunk_count++;
-
-            init_chunk(chunk, a, b);
-
-        }
-
-        else
-        {
-
-            return;
-
-        }
-
-    }
-
-    WorkerItem *item = &worker->item;
-
-    item->p = chunk->p;
-    item->q = chunk->q;
-    item->load = load;
-
-    for (int dp = -1; dp <= 1; dp++)
-    {
-
-        for (int dq = -1; dq <= 1; dq++)
-        {
-
-            Chunk *other = chunk;
-
-            if (dp || dq)
-                other = find_chunk(chunk->p + dp, chunk->q + dq);
-
-            if (other)
-            {
-
-                Map *block_map = malloc(sizeof(Map));
-
-                map_copy(block_map, &other->map);
-
-                Map *light_map = malloc(sizeof(Map));
-
-                map_copy(light_map, &other->lights);
-
-                item->block_maps[dp + 1][dq + 1] = block_map;
-                item->light_maps[dp + 1][dq + 1] = light_map;
-
-            }
-
-            else
-            {
-
-                item->block_maps[dp + 1][dq + 1] = 0;
-                item->light_maps[dp + 1][dq + 1] = 0;
-
-            }
-
-        }
-
-    }
-
-    chunk->dirty = 0;
-    worker->state = WORKER_BUSY;
-
-    cnd_signal(&worker->cnd);
-
-}
-
-static void ensure_chunks(Player *player)
-{
-
-    check_workers();
-    force_chunks(player);
-
-    for (int i = 0; i < WORKERS; i++)
-    {
-
-        Worker *worker = g->workers + i;
-
-        mtx_lock(&worker->mtx);
-
-        if (worker->state == WORKER_IDLE)
-            ensure_chunks_worker(player, worker);
-
-        mtx_unlock(&worker->mtx);
-
-    }
-    
-}
-
-static int worker_run(void *arg)
-{
-
-    Worker *worker = (Worker *)arg;
-
-    int running = 1;
-
-    while (running)
-    {
-
-        WorkerItem *item = &worker->item;
-
-        mtx_lock(&worker->mtx);
-
-        while (worker->state != WORKER_BUSY)
-            cnd_wait(&worker->cnd, &worker->mtx);
-
-        mtx_unlock(&worker->mtx);
-
-        if (item->load)
-            chunk_load(item);
-
-        compute_chunk(item);
-        mtx_lock(&worker->mtx);
-
-        worker->state = WORKER_DONE;
-
-        mtx_unlock(&worker->mtx);
-
-    }
-
-    return 0;
 
 }
 
@@ -1837,7 +1541,7 @@ static int render_chunks(Attrib *attrib, Player *player)
 
     int result = 0;
 
-    ensure_chunks(player);
+    force_chunks(player);
 
     int p = chunked(player->x);
     int q = chunked(player->z);
@@ -2994,20 +2698,6 @@ int main(int argc, char **argv)
     g->create_radius = CREATE_CHUNK_RADIUS;
     g->render_radius = RENDER_CHUNK_RADIUS;
     g->delete_radius = DELETE_CHUNK_RADIUS;
-
-    for (int i = 0; i < WORKERS; i++)
-    {
-
-        Worker *worker = g->workers + i;
-
-        worker->index = i;
-        worker->state = WORKER_IDLE;
-
-        mtx_init(&worker->mtx, mtx_plain);
-        cnd_init(&worker->cnd);
-        thrd_create(&worker->thrd, worker_run, worker);
-
-    }
 
     int running = 1;
 
